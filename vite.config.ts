87326b907e49239b444ae41f191acf516fae5db3
danzin <excl.d.k@gmail.com>
2023-12-06 11:46:28 +0200

    chore: Vitest testing setup complete

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -6,6 +9,11 @@ export default defineConfig({
   plugins: [react()],
   server:{
     host:true
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ["./src/__test__/setupTests.tsx"]
   },
   resolve: {
     alias: {
