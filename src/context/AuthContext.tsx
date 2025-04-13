5820bc5e87f1c3599052ee9eeeb8896fcc3859e9
danzin <excl.d.k@gmail.com>
2025-04-13 16:03:19 +0300

    build errors

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,6 @@
 import { getCurrentUser } from '@/lib/appwrite/api';
 import { IContextType, IUser } from '@/types';
 import {createContext, useContext, useEffect, useState} from 'react'
-import { useNavigate } from 'react-router-dom'
 
 export const INITIAL_USER = {
   id: '',
